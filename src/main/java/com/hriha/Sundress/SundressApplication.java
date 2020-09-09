0aba6dfa696fa3e656834f99f7bfd760ab997f63
Mykhailo Hriha <mykhailho.hriha@gmail.com>
2020-09-09 20:39:42 +0300

    first commit

---
+++ src/main/java/com/hriha/Sundress/SundressApplication.java
@@ -0,0 +1,13 @@
+package com.hriha.Sundress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SundressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SundressApplication.class, args);
+	}
+
+}
